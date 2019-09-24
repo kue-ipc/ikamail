3bcc02f6766242d722390f4072e3d37171873925
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-09-24 12:30:34 +0900

    LDAPのユーザーとグループを見に行けるように

--- app/models/ldap_group.rb
+++ app/models/ldap_group.rb
@@ -2,4 +2,13 @@ class LdapGroup < ActiveLdap::Base
   ldap_mapping dn_attribute: configurations['ldap']['group_dn'],
                prefix: configurations['ldap']['group_base'],
                classes: configurations['ldap']['group_classes']
+
+  has_many :primary_users, class_name: 'LdapUser',
+                           primary_key: 'gidNumber',
+                           foreign_key: 'gidNumber'
+
+  has_many :users, class_name: 'LdapUser',
+                   primary_key: 'uid',
+                   wrap: 'memberUid'
+
 end
--- app/models/ldap_user.rb
+++ app/models/ldap_user.rb
@@ -2,10 +2,12 @@ class LdapUser < ActiveLdap::Base
   ldap_mapping dn_attribute: configurations['ldap']['user_dn'],
                prefix: configurations['ldap']['user_base'],
                classes: configurations['ldap']['user_classes']
-  belongs_to :primary_group,
-             class_name: 'LdapGroup',
-             foreign_key: 'gidNumber',
-             primary_key: 'gidNumber'
-  belongs_to :groups,
-             many: 'memberUid'
+
+  belongs_to :primary_group, class_name: 'LdapGroup',
+                             primary_key: 'gidNumber',
+                             foreign_key: 'gidNumber'
+
+  belongs_to :groups, class_name: 'LdapGroup',
+                      primary_key: 'uid',
+                      many: 'memberUid'
 end
