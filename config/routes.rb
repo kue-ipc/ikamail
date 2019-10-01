2533d70dd3ad1076cebfd2958460fc6d282730b0
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-10-01 19:29:15 +0900

    コントローラーとか

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :recipient_lists
+  resources :recipient_lists do
+    resources :mail_groups, only: [:update, :destroy], controller: 'recipient_lists_mail_groups'
+  end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
