ed55769696eaaf5d97ccf1a0cf61bbd7cd4406cc
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-10-01 12:24:38 +0900

    bootstrap.nativを追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import bsn from 'bootstrap.native/dist/bootstrap-native-v4'
