1f055529472e317112ab1a24199bd82b2c3f4485
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-10-10 10:06:07 +0900

    wslでどうやってもselniumが動かないので、今は無視する。

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,8 +3,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |driver_option|
+  driven_by :selenium, using: :headless_chrome, screen_size: [1024, 768] do |driver_option|
     driver_option.add_argument('--no-sandbox')
     driver_option.add_argument('--disable-gpu')
+    driver_option.add_argument('--window-size=1024,768')
   end
 end
