4fefeafe9d9e0506e07a822653b2288ada459902
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-10-11 17:15:37 +0900

    宛先リストの表示の整理中

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,24 +7,24 @@ module ApplicationHelper
     if current_user.admin
       list << {
         path: admin_root_path,
-        label: t('path.admin_root'),
+        label: t('menu.path.admin_root'),
       }
     end
     list << {
       path: new_bulk_mail_path,
-      label: t('actions.new', model: t('activerecord.models.bulk_mail')),
+      label: t('menu.action.new', name: t('activerecord.models.bulk_mail')),
     }
     list << {
       path: bulk_mails_path,
-      label: t('actions.index', model: t('activerecord.models.bulk_mail')),
+      label: t('menu.action.index', name: t('activerecord.models.bulk_mail', count: 2)),
     }
     list << {
       path: recipient_lists_path,
-      label: t('actions.index', model: t('activerecord.models.recipient_list')),
+      label: t('menu.action.index', name: t('activerecord.models.recipient_list', count: 2)),
     }
     list << {
       path: bulk_mail_templates_path,
-      label: t('actions.index', model: t('activerecord.models.bulk_mail_template')),
+      label: t('menu.action.index', name: t('activerecord.models.bulk_mail_template', count: 2)),
     }
   end
 end
