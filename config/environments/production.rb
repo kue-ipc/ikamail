32abab509fef62806c566a6bf7e4704ab4e3f1db
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2019-12-26 15:42:08 +0900

    productionはdelayed_jobに

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # production queue delayed_job
+  config.active_job.queue_adapter = :delayed_job
 end
