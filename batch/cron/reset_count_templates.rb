8a1389f6e2655294d2d50cfd302afb977efff018
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-02-07 12:09:10 +0900

    バッチファイルについてジョブ登録に

--- batch/cron/reset_count_templates.rb
+++ batch/cron/reset_count_templates.rb
@@ -3,7 +3,5 @@
 
 if $0 == __FILE__
   Rails.logger.info("runner #{__FILE__}")
-  Template.find_each do |template|
-    template.update_column(:count, 0)
-  end
+  ResetCountTemplateJob.perform_later
 end
