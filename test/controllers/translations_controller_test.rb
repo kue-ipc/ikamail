1b42d37714e24dc5cf0da686d55dfecd79472def
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2020-04-09 13:33:49 +0900

    テストはしない、まだ

--- test/controllers/translations_controller_test.rb
+++ test/controllers/translations_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class TranslationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get translations_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get translations_index_url
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get translations_update_url
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get translations_update_url
+  #   assert_response :success
+  # end
 
 end
