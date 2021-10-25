b58f56dfc12684173bb5e2f4bb5b1bb5e8eab52d
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2021-10-25 10:01:46 +0900

    ロックを掛けるのと猶予は逆だった

--- app/jobs/reserved_delivery_job.rb
+++ app/jobs/reserved_delivery_job.rb
@@ -3,12 +3,15 @@ class ReservedDeliveryJob < ApplicationJob
 
   def perform(bulk_mail)
     return if bulk_mail.nil?
-    return unless bulk_mail.status_reserved?
-    # 1分の猶予を持つようにする。
-    return unless bulk_mail.reserved_at&.<=(Time.current.ago(1.minute))
 
-    bulk_mail.update(status: 'waiting')
-    ActionLog.create(bulk_mail: bulk_mail, action: 'deliver', comment: 'reserved')
-    BulkMailer.with(bulk_mail: bulk_mail).all.deliver_later
+    bulk_mail.with_lock do
+      return unless bulk_mail.status_reserved?
+      # 1分の猶予を持つようにする。
+      return unless bulk_mail.reserved_at&.<=(Time.current.since(1.minute))
+
+      bulk_mail.update(status: 'waiting')
+      ActionLog.create(bulk_mail: bulk_mail, action: 'deliver', comment: 'reserved')
+      BulkMailer.with(bulk_mail: bulk_mail).all.deliver_later
+    end
   end
 end
