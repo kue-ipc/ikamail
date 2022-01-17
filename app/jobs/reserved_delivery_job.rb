b78fb15c2ad2d8d69b5ae22cdcc8cfacb6c784eb
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2022-01-17 12:22:22 +0900

    ロックの範囲を変える

--- app/jobs/reserved_delivery_job.rb
+++ app/jobs/reserved_delivery_job.rb
@@ -9,9 +9,9 @@ class ReservedDeliveryJob < ApplicationJob
       # 1分の猶予を持つようにする。
       return unless bulk_mail.reserved_at&.<=(Time.current.since(1.minute))
 
-      bulk_mail.update(status: 'waiting')
-      ActionLog.create(bulk_mail: bulk_mail, action: 'deliver', comment: 'reserved')
-      BulkMailer.with(bulk_mail: bulk_mail).all.deliver_later
+      bulk_mail.update!(status: 'waiting')
     end
+    ActionLog.create(bulk_mail: bulk_mail, action: 'deliver', comment: 'reserved')
+    BulkMailer.with(bulk_mail: bulk_mail).all.deliver_later
   end
 end
