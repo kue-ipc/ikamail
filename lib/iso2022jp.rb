55d8b6b632e2c4aa4c39f5f9578c99652bcefbb5
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-03-20 16:59:55 +0900

    nkfを使用せずにencodeで変換する

--- lib/iso2022jp.rb
+++ lib/iso2022jp.rb
@@ -1,43 +1,53 @@
 # libary for ISO-2022-JP (JIS code)
 
-require 'nkf'
+# NKFではなくRuby本体のEncodingを使用する
+# 全角チルダと全角マイナスの扱いの違いに注意すること
 
 module Iso2022jp
-  NO_JIS_CHARS = %w[
-    丨 仡 伀 伃 伹 佖 侊 侒 侔 侚 俉 俍 俿 倞 倢 偀 偂 偆 偰 傔 僘 兊 兤 冝 冾
-    刕 劜 劦 劯 勀 勛 匀 卲 厓 厲 叝 咜 咩 哿 喆 坥 垬 埇 埈 墲 夋 奓 奛 奝 奣
-    妤 妺 孖 寀 寘 尞 岦 岺 崧 嵂 嵭 嶸 嶹 巐 弡 弴 彅 彧 忞 恝 悊 惕 惞 惲 愑
-    愰 愷 憘 戓 抦 揵 摠 撝 擎 昀 昉 昕 昞 昤 昮 昱 晗 晙 晳 暙 暠 暲 暿 曺 曻
-    朎 杦 枻 柀 桄 桒 棈 棏 楨 榘 槢 樰 橆 橳 橾 櫤 毖 氿 汜 汯 沆 泚 洄 浯 涇
-    涖 涬 淏 淼 渧 渹 渼 湜 溿 澈 澵 濵 瀅 瀇 炅 炫 焄 焏 煆 煇 煜 燁 燾 犱 犾
-    猤 獷 玽 珉 珒 珖 珣 珵 琇 琦 琩 琪 琮 瑢 璉 璟 甯 畯 皛 皜 皦 睆 砡 硎 硤
-    硺 禔 禛 竑 竫 箞 絈 絜 綷 繒 纊 罇 羡 茁 荿 菇 菶 葈 蒴 蓜 蕓 蕙 蕫 裵 褜
-    訒 訷 詹 誧 誾 諟 諶 譓 賰 贒 軏 遧 鄧 釗 釚 釞 釤 釥 釭 釮 鈆 鈊 鈐 鈹 鈺
-    鈼 鉀 鉎 鉑 鉙 鉧 鉷 鉸 銈 銧 鋐 鋓 鋕 鋗 鋙 鋠 鋧 鋹 鋻 鋿 錂 錝 錞 錡 錥
-    鍈 鍗 鍰 鎤 鏆 鏞 鏸 鐱 鑅 鑈 隝 隯 霳 靃 靏 靕 顗 顥 餧 驎 髜 魵 鮏 鮱 鮻
-    鰀 鵫 鵰 鸙
-    ￤
-  ].freeze
+  # EUC-JPに変化された文字が来る場合があるため、UTF-8にしてから処理すること
+  FB_PROCS = {
+    skip: ->(_) { '' },
+    html: ->(c) { '&#%d;' % c.encode(Encoding::UTF_8).ord },
+    xml: ->(c) { '&#x%X;' % c.encode(Encoding::UTF_8).ord },
+    perl: ->(c) { '\\x{%X}' % c.encode(Encoding::UTF_8).ord },
+    java: ->(c) { c.encode(Encoding::UTF_16LE).unpack('v*').map { |n| '\\u%04X' % n }.join },
+    subchar: ->(_) { '?' },
+  }
 
   module_function # rubocop:disable Style/AccessModifierDeclarations
 
   def check_unconvertible_chars(str, strict: true)
     no_amp_str = str.gsub('&', '&amp;')
-    conv_str = double_conv_jis(no_amp_str, fallback: :xml, cp932ext: !strict)
-    list = conv_str.scan(/&\#x(\h{1,6});/)
-      .map { |m| m[0].to_i(16).chr(Encoding::UTF_8) }
-    list.concat(conv_str.scan(Regexp.union(NO_JIS_CHARS))) if strict
-    list
+    conv_str = double_conv_jis(no_amp_str, fallback: :xml, cp932: !strict)
+
+    conv_str.scan(/&\#x(\h{1,6});/).map { |m| m[0].to_i(16).chr(Encoding::UTF_8) }
   end
 
-  def double_conv_jis(str, fallback: :skip, cp932ext: false)
-    fallback = fallback.intern
-    raise ArgumentError, 'invalid fallback' unless %i[skip html xml perl java subchar].include?(fallback)
+  # fallback: 存在しない場合
+  # cp932: CP932で拡張された文字を含める (CP50220 or CP50221)
+  # x0201: JIS X 0201(半角カタカナ) を JIS X 0208(全角カタカナ) に変換する
+  def double_conv_jis(str, fallback: :skip, cp932: true, x0201: true)
+    str = str.dup
+    fb_proc = FB_PROCS[fallback]
+    raise ArgumentError, 'invalid fallback' unless fb_proc
 
-    nkf_opts = ['-W', '-j']
-    nkf_opts << "--fb-#{fallback}" if fallback != :skip
-    nkf_opts << '--no-cp932ext' unless cp932ext
-
-    NKF.nkf('-J -w', NKF.nkf(nkf_opts.join(' '), str))
+    encode =
+      if cp932
+        # マイナス -> 全角マイナス
+        # 波ダッシュ -> 全角チルダ
+        str.tr!("\u2212\u301C", "\uFF0D\uFF5E")
+        if x0201
+          # 半角カタカナを全角カタカナに変換する
+          Encoding::CP50220
+        else
+          # mail-iso-2022-jp が使用するエンコードと同じ
+          Encoding::CP50221
+        end
+      else
+        str.tr!("\uFF0D\uFF5E", "\u2212\u301C")
+        str.gsub!(/[\uFF61-\uFF9F]+/) { |s| s.unicode_normalize(:nfkc) } if x0201
+        Encoding::ISO_2022_JP
+      end
+    str.encode(encode, Encoding::UTF_8, fallback: fb_proc).encode(Encoding::UTF_8)
   end
 end
