c6e4d0fa3dc4634dafab2c45fab60b101a1fec2a
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-08-01 14:17:06 +0900

    テストを作成

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,13 +1,64 @@
-require "test_helper"
+require 'test_helper'
 
 class SearchesControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get searches_new_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  class SignInAdmin < SearchesControllerTest
+    setup do
+      sign_in users(:admin)
+    end
+
+    test 'should get new' do
+      get new_search_url
+      assert_response :success
+    end
+
+    test 'redirect to get new INSTEAD OF show seach' do
+      get search_url
+      assert_redirected_to new_search_path
+    end
+
+    test 'should create search' do
+      post search_url, params: {query: 'user01'}
+      assert_response :success
+    end
   end
 
-  test "should get create" do
-    get searches_create_url
-    assert_response :success
+  class SignInUser < SearchesControllerTest
+    setup do
+      sign_in users(:user01)
+    end
+
+    test 'should get new' do
+      get new_search_url
+      assert_response :success
+    end
+
+    test 'redirect to get new INSTEAD OF show seach' do
+      get search_url
+      assert_redirected_to new_search_path
+    end
+
+    test 'should create search' do
+      post search_url, params: {query: 'user01'}
+      assert_response :success
+    end
+  end
+
+  class Anonymous < SearchesControllerTest
+    test 'redirect to login INSTEAD OF get index' do
+      get new_search_url
+      assert_redirected_to new_user_session_path
+    end
+
+    test 'redirect to get new INSTEAD OF show seach' do
+      get search_url
+      assert_redirected_to new_search_path
+    end
+
+    test 'redirect to login INSTEAD OF get new' do
+      post search_url, params: {query: 'user01'}
+      assert_redirected_to new_user_session_path
+    end
   end
 end
