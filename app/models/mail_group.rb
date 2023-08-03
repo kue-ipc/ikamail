e5cd2c5f141b76556144d303d0195e588a3cd6cd
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-08-03 13:41:24 +0900

    distinctを追加

--- app/models/mail_group.rb
+++ app/models/mail_group.rb
@@ -1,13 +1,14 @@
 class MailGroup < ApplicationRecord
   has_and_belongs_to_many :recipient_lists # rubocop: disable Rails/HasAndBelongsToMany
-
   has_many :mail_memberships, dependent: :destroy
+  has_many :mail_users, -> { distinct }, through: :mail_memberships
+
+  # rubocop: disable Rails/HasManyOrHasOneDependent
   has_many :primary_memberships, -> { where(primary: true) }, class_name: 'MailMembership', inverse_of: :mail_group
   has_many :secondary_memberships, -> { where(primary: false) }, class_name: 'MailMembership', inverse_of: :mail_group
-
-  has_many :mail_users, through: :mail_memberships
   has_many :primary_users, through: :primary_memberships, source: :mail_user
   has_many :secondary_users, through: :secondary_memberships, source: :mail_user
+  # rubocop: enable Rails/HasManyOrHasOneDependent
 
   def to_s
     if display_name.present?
