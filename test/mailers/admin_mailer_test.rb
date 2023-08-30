f653c9eb421c9d5762b590128e7193bc1e2b8014
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2023-08-30 16:23:03 +0900

    rubocop -a

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -1,11 +1,11 @@
-require "test_helper"
+require 'test_helper'
 
 class AdminMailerTest < ActionMailer::TestCase
-  test "job_failure" do
+  test 'job_failure' do
     mail = AdminMailer.with(job: 'job', job_id: 'job_id', time: 'time', exception: 'exception').job_failure
-    assert_equal "【一括メールシステム管理通知】ジョブ失敗", mail.subject
-    assert_equal ["admin@example.jp"], mail.to
-    assert_equal ["no-reply@example.jp"], mail.from
+    assert_equal '【一括メールシステム管理通知】ジョブ失敗', mail.subject
+    assert_equal ['admin@example.jp'], mail.to
+    assert_equal ['no-reply@example.jp'], mail.from
 
     assert_match <<~MESSAGE.gsub(/\R/, "\r\n"), mail.body.encoded
       一括メールシステムで、下記のジョブが失敗しました。
@@ -20,5 +20,4 @@ class AdminMailerTest < ActionMailer::TestCase
       http://ikamail.example.jp/
     MESSAGE
   end
-
 end
