2fbf41d8fbd9a4bb81b94733fa5c70c2acd0c23c
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2024-02-09 14:22:56 +0900

    何もしないスキーマを追加

---
+++ db/migrate/20240209045043_rename_templates_to_mail_templates.rb
@@ -0,0 +1,5 @@
+class RenameTemplatesToMailTemplates < ActiveRecord::Migration[7.0]
+  def change
+    # do nothing
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_27_005012) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_09_045043) do
   create_table "action_logs", charset: "utf8mb4", collation: "utf8mb4_general_ci", options: "ENGINE=InnoDB ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.bigint "bulk_mail_id", null: false
     t.bigint "user_id"
