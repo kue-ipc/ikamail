7e4f5be06f56f887eb3b3829eb6cfcee9a1829ee
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-12-25 14:04:50 +0900

    ファイル名が間違っていた

--- config/initializers/fix_redis_cache_store.rb
+++ config/initializers/fix_redis_cache_store.rb
@@ -1,6 +1,6 @@
 # fix RedisCacheStore
 # TODO: Remove this patch when Rails 8.1.2 or later is required.
 if ENV.fetch("RAILS_CACHE_STORE", Settings.cache&.store) == "redis"
-  require "fix_cache_redis_store"
+  require "fix_redis_cache_store"
   FixRedisCacheStore.load
 end
