eb9222d4d5395674e2a741530f7a605067225797
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2025-12-26 13:33:01 +0900

    Revert "個別のrequireが必要"
    This reverts commit 2513f9cdf70694459fe272c784e049ad6c302737.

--- config/initializers/mission_control.rb
+++ config/initializers/mission_control.rb
@@ -1,7 +1,6 @@
 # for mission_control-jobs
 
 if Rails.application.config.active_job.queue_adapter == :solid_queue
-  require "mission_control/jobs"
   # mission_control-jobs configuration
   Rails.application.config.mission_control.jobs.base_controller_class = "AdminController"
   Rails.application.config.mission_control.jobs.http_basic_auth_enabled = false
