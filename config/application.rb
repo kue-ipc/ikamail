c155b640dbd654d0e2f97084c11cf6c9fda38cb6
IGARASHI Makoto <igarashi@kyokyo-u.ac.jp>
2026-02-03 09:37:53 +0900

    v1.0.3

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 
 module Ikamail
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
